7ee8984caba2bfb55cf28f8ea93a8d002d79709d
Tereza Muzikova <tereza.muzikova@applifting.cz>
2025-08-12 16:12:52 +0200

    feat: embed 3 games

--- src/components/Embed/EmbedFrame.jsx
+++ src/components/Embed/EmbedFrame.jsx
@@ -4,15 +4,18 @@ import React from "react";
 export const EmbedFrame = () => {
   
 
-  const src =  "http://localhost:3002/game/znate-se";
+  const src1 =  "http://localhost:3002/game/miliardar";
+  const src2 =  "http://localhost:3002/game/sluchatka";
+  const src3 =  "http://localhost:3002/game/co-hodi-vic";
 
-  console.log(src)
+  
 
   return (
+    <>
     <div style={{ width: "100%", height: "80vh", display: "flex", justifyContent: "center" }}>
       <iframe
         title="Milionář"
-        src={src}
+        src={src1}
         width="100%"
         height="100%"
         style={{ border: "2px solid #ccc", borderRadius: 8 }}
@@ -20,5 +23,27 @@ export const EmbedFrame = () => {
         allow="fullscreen; clipboard-read; clipboard-write"
       />
     </div>
+     <div style={{ width: "100%", height: "80vh", display: "flex", justifyContent: "center" }}>
+     <iframe
+       title="Milionář"
+       src={src2}
+       width="100%"
+       height="100%"
+       style={{ border: "2px solid #ccc", borderRadius: 8 }}
+       frameBorder="0"
+       allow="fullscreen; clipboard-read; clipboard-write"
+     />
+   </div> <div style={{ width: "100%", height: "80vh", display: "flex", justifyContent: "center" }}>
+      <iframe
+        title="Milionář"
+        src={src3}
+        width="100%"
+        height="100%"
+        style={{ border: "2px solid #ccc", borderRadius: 8 }}
+        frameBorder="0"
+        allow="fullscreen; clipboard-read; clipboard-write"
+      />
+    </div>
+    </>
   );
 }; 
