45037c80a17575eb218b369630deefac5039a462
Tereza Muzikova <tereza.muzikova@applifting.cz>
2025-08-12 16:15:30 +0200

    feat: embed restricted game

--- src/components/Embed/EmbedFrame.jsx
+++ src/components/Embed/EmbedFrame.jsx
@@ -7,7 +7,7 @@ export const EmbedFrame = () => {
   const src1 =  "http://localhost:3002/game/miliardar";
   const src2 =  "http://localhost:3002/game/sluchatka";
   const src3 =  "http://localhost:3002/game/co-hodi-vic";
-
+  const src4 =  "http://localhost:3002/game/znate-se";
   
 
   return (
@@ -37,6 +37,17 @@ export const EmbedFrame = () => {
       <iframe
         title="Milionář"
         src={src3}
+        width="100%"
+        height="100%"
+        style={{ border: "2px solid #ccc", borderRadius: 8 }}
+        frameBorder="0"
+        allow="fullscreen; clipboard-read; clipboard-write"
+      />
+    </div>
+    <div style={{ width: "100%", height: "80vh", display: "flex", justifyContent: "center" }}>
+      <iframe
+        title="Milionář"
+        src={src4}
         width="100%"
         height="100%"
         style={{ border: "2px solid #ccc", borderRadius: 8 }}
